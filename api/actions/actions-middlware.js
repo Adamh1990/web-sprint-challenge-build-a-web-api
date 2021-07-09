00e501f15279c745f0b1084207259a3efb51a8e3
Adamh1990 <68036525+Adamh1990@users.noreply.github.com>
2021-07-09 15:40:20 -0500

    26/27

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to actions
+function logger(req, res, next) {
+    const timestamp = new Date().toLocaleString()
+    const method = req.method
+    const url = req.originalUrl
+    console.log(`[${timestamp}] ${method} to ${url}`)
+    next()
+}
+const Actions = require('./actions-model')
+
+function validateActionsId(req, res, next){
+    Actions.get(req.params.id)
+        .then(resp => {
+            if (resp){
+                next()
+            }else res.status(404).json({message: `Action with that ID doesn't exist in the database`})
+        })
+        .catch(err => {
+            console.err('error validating id: ', err)
+        })
+}
+
+function validateActions(req, res, next) {
+    if (!req.body.project_id || !req.body.description || !req.body.notes) {
+        res.status(400).json({
+            message: "Project name and description required"
+        })
+    } else next()
+}
+
+
+module.exports = {
+    logger,
+    validateActionsId,
+    validateActions
+}
--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const { logger } = require('./actions/actions-middlware')
 const server = express();
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
+server.use(express.json());
+server.use(logger)
+server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>OMG LET'S DO THIS!!</h2>`);
+});
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
